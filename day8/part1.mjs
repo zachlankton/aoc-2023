d47ecc8352a6e479e03a7c2ded0d3654ad825c95
Zach Lankton <zachlankton@gmail.com>
2023-12-08 01:10:35 -0500

    Finished Part1 Day 8

--- day8/part1.mjs
+++ day8/part1.mjs
@@ -5,6 +5,9 @@ const x = fs.readFileSync("input.txt");
 const input = x.toString();
 
 const lines = input.split("\n");
+const instructions = lines.shift().split("");
+lines.shift();
+const whatever = {};
 
 scanLines();
 
@@ -12,20 +15,23 @@ function scanLines() {
   let total = 0;
   lines.forEach((line) => {
     if (!line) return;
-
-    // total += num;
+    const spl = line.split(" = ");
+    const loc = spl[0];
+    const [L, R] = spl[1].slice(1, 9).split(", ");
+    whatever[spl[0]] = { L, R };
   });
-  console.log(total);
-}
 
-function stub1() {
-  console.log("stub1");
+  const instrLen = instructions.length;
+  let currentNode = "AAA";
+  let currentInstrIndex = 0;
+  let count = 0;
+  while (currentNode != "ZZZ") {
+    count += 1;
+    if (currentInstrIndex === instrLen) currentInstrIndex = 0;
+    let currentInstruction = instructions[currentInstrIndex];
+    currentNode = whatever[currentNode][currentInstruction];
+    currentInstrIndex += 1;
+  }
 
-  // stop prettier from collapsing my whitespace
-}
-
-function stub2() {
-  console.log("stub2");
-
-  // stop prettier from collapsing my whitespace
+  console.log(count);
 }
